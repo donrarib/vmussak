00bfc9b07cc4c4b3748bf7c78ebbe87877860628
donrarib <rrdonato@gmail.com>
2020-12-15 10:01:18 -0300

    Testing heroku

--- core/typecustomer/controller.js
+++ core/typecustomer/controller.js
@@ -7,20 +7,30 @@ module.exports = {
 
 // getall method
 async function getAll(req, res){
-  try {
-    let result = await model.findAll({
-      raw: true,
-      attributes: ['id', 'description']
-    });
+  let result = await model.findAll({
+    raw: true,
+    attributes: ['id', 'description']
+  });
 
-    res.status(200).json({
-      content: result
-    });
+  res.status(200).json({
+    content: result
+  });
 
-  } catch (e) {
-    res.status(500).json({
-      error: error
-    })
-  }
+  //
+  // try {
+  //   let result = await model.findAll({
+  //     raw: true,
+  //     attributes: ['id', 'description']
+  //   });
+  //
+  //   res.status(200).json({
+  //     content: result
+  //   });
+  //
+  // } catch (e) {
+  //   res.status(500).json({
+  //     error: error
+  //   })
+  // }
 
 }
